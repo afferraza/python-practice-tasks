25e95eb80c5c51788361dbc84ab518d83d1a149b
affer raza <a.raza@ssidecisions.com>
2019-07-09 16:29:40 +0500

    Task 11 & 12

---
+++ Task11.py
@@ -0,0 +1,15 @@
+"""
+Write a Python program to remove the parenthesis area in a string.
+
+Sample data : ["example (.com)", "w3resource", "github (.com)", "stackoverflow (.com)"]
+
+"""
+
+import re
+pattern = re.compile("\([^()]*\)")
+
+sample_data = ["example (.com)", "w3resource",
+               "github (.com)", "stackoverflow (.com)"]
+
+for data in sample_data:
+    print(re.sub(pattern, '', data))
---
+++ Task12.py
@@ -0,0 +1,12 @@
+"""
+Write a Python program to find urls in a string and output all urls as list.
+"""
+
+import re
+pattern = re.compile("\([^()]*\)")
+
+pattern = re.compile("https*://[\w.@_!#$%^&*()<>?/\|}{~:\w]*\S")
+
+sample_data = "My Profile: https://auth.geeksforgeeks.org/user/Chinmoy%20Lenka/articles in the portal of http://www.geeksforgeeks.org"
+
+print(re.findall(pattern,  sample_data))
